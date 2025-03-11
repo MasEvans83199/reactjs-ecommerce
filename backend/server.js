8194da428055a673de3ccc6a135d2bbb2dce7299
Mason Evans <masevans83199@gmail.com>
2025-03-11 16:23:20 -0500

    express server setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(express.json());
+
+app.get("/ping", (req, res)=> {
+    console.log("Received a ping... responding with a pong...");
+    res.send("PONG back at you...");
+});
+
+app.listen(PORT, () =>
+     console.log(`MERN server is listening on port ${PORT}...`)
+);
